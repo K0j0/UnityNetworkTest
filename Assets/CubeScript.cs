20f426c9687ff9fb0dff91e76af00a7aa567b0a7
Kojo Kumah <info@kojokumah.com>
2015-01-15 20:20:48 -0800

    Added some logs, started generalizing network stuff a bit

--- Assets/CubeScript.cs
+++ Assets/CubeScript.cs
@@ -1,15 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Text.RegularExpressions;
 
 public class CubeScript : MonoBehaviour {
+	public bool isServer;
+	bool useNat;
 
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log ("My os is " + SystemInfo.operatingSystem);
+		bool onWindows = Regex.IsMatch(SystemInfo.operatingSystem, "Windows");
+		bool onMac = Regex.IsMatch(SystemInfo.operatingSystem, "Mac OS X");
+		Debug.Log ("On windows? " + onWindows + " On a Mac? " + onMac);
+		Debug.Log("My ip address is " + Network.player.ipAddress);
+		
+		
+		if(isServer){
+			useNat = !Network.HavePublicAddress();
+			Debug.Log("Has public ip? " + Network.HavePublicAddress() + " Use NAT? " + useNat);
+			Network.InitializeServer(8, 8001, useNat);			
+			
+		
+		} else{
+			// connect to my mac
+			if(onWindows){
+				// local
+				if(useNat) Network.Connect("192.168.0.15", 8001);				
+				// over the internet
+				else;
+			}
+			// connect to my PC
+			else if(onMac){
+				// local
+				if(useNat) Network.Connect("192.168.0.11", 8001);
+				// over the internet
+				else;
+			}
+			
+		}
+		
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	
+	void OnServerInitialized() {
+		Debug.Log("Server initialized and ready");
+		Debug.Log("My external ip address is " + Network.player.externalIP);
 	}
 }
