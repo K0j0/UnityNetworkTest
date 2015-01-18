64171682f3067ba709f47a78dd495fffde8b96b2
Kojo Kumah <info@kojokumah.com>
2015-01-17 22:45:47 -0800

    Stopped ignoring scripts folder

---
+++ Assets/Scripts/ServerManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServerManager : MonoBehaviour {
+	bool useNat;
+	
+	// Use this for initialization
+	void Start () {		
+		useNat = !Network.HavePublicAddress();
+		Debug.Log("Has public ip? " + Network.HavePublicAddress() + " Use NAT? " + useNat);
+		Network.InitializeServer(8, 8001, useNat);
+		MasterServer.RegisterHost("KojoTest", "game1");
+	}
+	
+	void OnServerInitialized() {
+		Debug.Log("Server initialized and ready");
+		Debug.Log("My external ip address is " + Network.player.externalIP);
+	}
+}
